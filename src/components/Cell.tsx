65f03a56b09fb245869287c47f3cbff2b1cbd0af
Art PC <art@example.com>
2025-05-20 20:06:22 +0300

    Re-design 2

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BLOCK_SIZE } from '../game/constants';
+import { useMediaQuery } from 'react-responsive';
 
 interface CellProps {
   color: string;
 }
 
 const Cell: React.FC<CellProps> = ({ color }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <div 
       style={{
-        width: BLOCK_SIZE,
-        height: BLOCK_SIZE,
+        width: !isMobile ? BLOCK_SIZE : BLOCK_SIZE/2,
+        height: !isMobile ? BLOCK_SIZE : BLOCK_SIZE/2,
         backgroundColor: color,
         border: '1px solid rgba(0, 0, 0, 0.1)'
       }}
