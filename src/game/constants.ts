a3a7b2dfd60aa34355edc054d1b42b934dc25322
Artem <tetris@game.com>
2025-05-20 12:58:37 +0300

    Fix 11

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -5,7 +5,7 @@ export const NEXT_COLS = 4;
 export const NEXT_ROWS = 4;
 
 export const SHAPES = [
-  [],
+ /* [],*/
   [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]], // I
   [[2, 0, 0], [2, 2, 2], [0, 0, 0]], // J
   [[0, 0, 3], [3, 3, 3], [0, 0, 0]], // L
