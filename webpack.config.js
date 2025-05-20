ae8b81e2164d98f1ab3d55e95d3447fdcf1ea65c
Artem <tetris@game.com>
2025-05-20 12:06:41 +0300

    Fix 6

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -26,17 +26,17 @@ module.exports = {
     ],
   },
   plugins: [
-    /*
+    
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
     }),    
-    */
+    
     new CopyWebpackPlugin({
       patterns: [
         { from: 'src/404.html', to: '404.html' }
       ]
-    })   
+    }),   
   ],
   devServer: {
     static: {
