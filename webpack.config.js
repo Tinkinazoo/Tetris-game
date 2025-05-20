7e8e5129d0bdc5de36860706c0386dc1c1e25503
Artem <tetris@game.com>
2025-05-20 11:49:23 +0300

    Fix 2

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: /*process.env.NODE_ENV === 'production' ?*/ '/Tetris-game/' /*: '/' */,
+    publicPath: '/Tetris-game/', /* '/' */
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
