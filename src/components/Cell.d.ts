e9a26e85ea995b57ceb53a9a732ee39c47812130
Artem <tetris@game.com>
2025-05-23 11:06:32 +0300

    Updates

--- src/components/Cell.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface CellProps {
-    color: string;
-}
-declare const Cell: React.FC<CellProps>;
-export default Cell;
--- src/components/GameController.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const GameController: React.FC;
-export default GameController;
